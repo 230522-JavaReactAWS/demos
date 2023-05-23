3dbd0023a3d3480b8a90e48678ebbaec81d48b95
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-05-23 11:11:45 -0400

    added HelloWorld

---
+++ HelloWorld/src/com/revature/Launcher.java
@@ -0,0 +1,23 @@
+package com.revature;
+
+public class Launcher {
+
+    //This is the main method - the starting/entry point for any Java application
+    public static void main(String[] args){
+
+        //a print statement allows us to "print" something to the console
+        //This lets us make stuff appear in the box that pops up when you run your application
+        System.out.println("Hello World!");
+
+        /*
+         System = Console. This system.
+         out = output, defining some output
+         println = print this on a new line.
+          */
+
+        //sout is the shortcut for making a print statement
+        System.out.println("I was made with a shortcut!");
+
+    }
+
+}
