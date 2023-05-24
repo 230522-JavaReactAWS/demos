3121124d37eb8d2fd5490725c4cfeb1ef8f54e84
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-05-24 16:00:57 -0400

    Showed an Interface in HelloAbstract

--- HelloAbstract/src/com/revature/Launcher.java
+++ HelloAbstract/src/com/revature/Launcher.java
@@ -1,11 +1,18 @@
 package com.revature;
 
+import com.revature.models.Lion;
+
 public class Launcher {
 
     public static void main(String[] args) {
 
+        Lion l = new Lion();
 
+        l.move();
 
     }
+
+
+
 
 }
