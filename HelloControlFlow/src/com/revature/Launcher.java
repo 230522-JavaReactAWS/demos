91748ed8c300627daaae9e61cbc9275513fec8ae
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-05-24 11:02:43 -0400

    saw for loops in HelloControlFlow

--- HelloControlFlow/src/com/revature/Launcher.java
+++ HelloControlFlow/src/com/revature/Launcher.java
@@ -57,6 +57,42 @@ public class Launcher {
 
         System.out.println("Do-While Loop has broken!");
 
+        System.out.println("==============================(For Loops)");
+
+        //basic for loop--/
+
+        //"for as long as this int i, which starts at 0, is less than 10, increment it once per loop"
+        //so in essence, we've written a loop that will run some code 10 times.
+        for(int i = 0; i < 10; i++){
+            //every time this loop loops, this code will run
+            System.out.println("Our loop variable is: " + i);
+        }
+
+        //more interesting use case of for loops--/
+
+        //This is an Array, which is basically a list of values. In this case, an Array of chars
+        //In other words, a char Array
+        char[] myName = {'B', 'E', ' ', 'N', 'A', 'A', 'A'};
+
+        //Arrays are INDEXED, so we can retrieve individual values using their index
+        System.out.println(myName[0]); //Arrays are ZERO INDEXED. The first value is at index zero.
+        System.out.println(myName[6]); //Last value. NOT 7, despite there being 7 values. (it goes 0-6)
+        System.out.println("----");
+
+        //I'm going to use a for loop to iterate through the Array, and print out the letters of my name
+
+        //"As long as i is less than the number of values in the myName Array, run this block and increment"
+        for(int i = 0; i < myName.length; i++){
+
+            if(myName[i] == ' ' || myName[i] == 'A'){
+                continue; //continue causes the loop to skip its current iteration (its current loop)
+            }
+
+            //if the if case is not met, we will print out the char
+            System.out.println(myName[i]);
+
+        }
+
     }
 
 }
