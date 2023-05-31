5c5e86fd1a4bf2a2f9cf55710bdf6986ad88f191
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-05-31 12:22:33 -0400

    showed relationships, select, update

---
+++ Week2/DML_DATATYPES.sql
@@ -0,0 +1,111 @@
+--I want to create two tables with a relationship
+--I can accomplish this with primary keys and foreign keys
+--Foreign keys of one table point to primary keys of another table. THIS ESTABLISHES A RELATIONSHIP
+
+--This schema will track employees and their roles
+
+--roles table
+CREATE TABLE roles(
+
+	role_id serial PRIMARY KEY, --The primary key UNIQUELY IDENTIFIES each record (row) in a table 
+	--serial is an auto-incrementing int data type - perfect primary keys, which must be unique
+	--whenever we insert a new record, the primary key will get populated and incremented for us
+	role_title TEXT UNIQUE, --now, every role must have a different title from each other
+	role_salary int check(role_salary > 20000) --thanks to the check constraint, salaries cannot be less than 20000. 
+
+);
+
+
+--employees table
+CREATE TABLE employees(
+
+	employee_id serial PRIMARY KEY, --same as above. unique identifier that automatically increments
+	first_name TEXT NOT NULL, --every employee must have a first name (NOT NULL)
+	last_name TEXT NOT NULL,
+	role_id_fk int REFERENCES roles(role_id) --this is a FOREIGN KEY. (note the "references" keyword)
+	/* this is saying "every employee has a role." This forieng key point to the primary key in the roles table
+	     THIS IS HOW WE ESTABLISH RELATIONSHIPS BETWEEN TABLES
+	     Now, every employee must have a role, and a role can have one, many, or no employees related to it*/
+
+);
+
+
+--DML------------------------------
+
+--Data Manipulation Language - SELECT, INSERT, UPDATE, DELETE (SELECT may also be considered DQL as opposed to DML)
+
+
+/*INSERT some data into my tables
+  
+  we specify what table and columns we're filling with data 
+  (we don't need to put a value for the auto-incrementing serial)
+  and then, we specify what data we actually want to insert
+  each pair of parenthesis will be a different record in the table */
+
+INSERT INTO roles(role_title, role_salary)
+VALUES ('Manager', 100000), 
+	   ('Cashier', 40000), 
+       ('Fry Cook', 50000), 
+       ('Marketing Director', 100000);
+
+--we can use SELECT to view data in the table
+SELECT * FROM roles; -- * means "everything". we're saying "select everything from the roles table"
+
+
+INSERT INTO employees(first_name, last_name, role_id_fk)
+VALUES ('Eugene', 'Krabs', 1),
+	   ('Spongebob', 'Squarepants', 3),
+	   ('Squidward', 'Tentacles', 2),
+	   ('Sheldon', 'Plankton', 1);
+
+
+SELECT * FROM employees
+
+--The WHERE CLAUSE-----------------------------
+
+--The WHERE clause in a SELECT statement lets us further filter the data that comes back
+
+--all roles where role_salary is 100,000 (=)
+SELECT * FROM roles WHERE role_salary = 100000;
+
+--all roles where role_salary is NOT 100,000 (!=)
+SELECT * FROM roles WHERE role_salary != 100000;
+
+--all roles where role_salary is less than 100,000 (<)
+SELECT * FROM roles WHERE role_salary < 100000;
+
+--all employees who have a first name starting with 'S' (LIKE %)
+SELECT * FROM employees WHERE first_name LIKE 'S%';
+--employees who's names start with S (and then the rest doesn't matter)
+
+--all employees with a salary BETWEEN 30000 and 70000 (BETWEEN AND)
+SELECT * FROM roles WHERE role_salary BETWEEN 30000 AND 70000;
+
+--all employees names Eugene OR Sheldon (OR)
+SELECT * FROM employees WHERE first_name = 'Eugene' OR first_name = 'Sheldon'; 
+
+--the same as above, but with the IN operator
+SELECT * FROM employees WHERE first_name IN ('Eugene', 'Sheldon'); --better if there are many values to choose from 
+
+
+--ORDER BY-----------------------------
+
+SELECT * FROM roles ORDER BY role_salary; --by default, it orders by increasing order
+
+--we can specify "desc" for descending order
+SELECT * FROM employees ORDER BY first_name DESC;
+
+
+
+
+--UPDATE--------------------
+
+--We can use the UPDATE command to change values in our tables
+
+--lets say want to change an employees last name (maybe they got married or sumn)
+UPDATE employees SET last_name = 'Plankton' WHERE first_name = 'Sheldon';
+--NOTE: the where clause is important here, otherwise everybody's last name would change
+
+
+
+
