2a0c678cde3af43436aca9e40d466b2cdd3c82e5
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-06-01 14:25:47 -0400

    completed Employee Class

--- p0demo/P0demo/src/main/java/com/revature/models/Employee.java
+++ p0demo/P0demo/src/main/java/com/revature/models/Employee.java
@@ -44,6 +44,56 @@ public class Employee {
         this.role_id_fk = role_id_fk;
     }
 
-    
+    //getters and setters
+    public int getEmployee_id() {
+        return employee_id;
+    }
 
+    public void setEmployee_id(int employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public int getRole_id_fk() {
+        return role_id_fk;
+    }
+
+    public void setRole_id_fk(int role_id_fk) {
+        this.role_id_fk = role_id_fk;
+    }
+
+    //toString() so that we can print out our Employee objects
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employee_id=" + employee_id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", role=" + role +
+                ", role_id_fk=" + role_id_fk +
+                '}';
+    }
 }
