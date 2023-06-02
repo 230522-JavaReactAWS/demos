c0ee497c74e3ef00228a531f6ba5c9bf583a3b47
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-06-02 11:57:12 -0400

    Finished select all Employee method

--- p0demo/P0demo/src/main/java/com/revature/daos/EmployeeDAO.java
+++ p0demo/P0demo/src/main/java/com/revature/daos/EmployeeDAO.java
@@ -3,9 +3,7 @@ package com.revature.daos;
 import com.revature.models.Employee;
 import com.revature.utils.ConnectionUtil;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class EmployeeDAO implements EmployeeDAOInterface{
@@ -16,6 +14,47 @@ public class EmployeeDAO implements EmployeeDAOInterface{
 
         //Instantiate a Connection object so that we can talk to the DB
         try(Connection conn = ConnectionUtil.getConnection()){
+
+            //A String that represents our SQL statement
+            String sql = "SELECT * FROM employees";
+
+            /* No variables are needed in the SQL query above. No need for a PreparedStatement!
+             Instead, we'll use a regular Statement object
+             BIG PICTURE: If we have no variables, Statement is fine, otherwise we need PreparedStatement */
+            Statement s = conn.createStatement();
+
+            //Now, execute the query, and save the results in a ResultSet
+            ResultSet rs = s.executeQuery(sql);
+
+            //Instantiate an empty ArrayList that will hold our incoming Employee data
+            ArrayList<Employee> employeeList = new ArrayList<>();
+
+            //Instantiate a RoleDAO so we can get a Role object for our Employee objects
+            RoleDAO rDAO = new RoleDAO();
+
+            /* use rs.next() in a while loop to create a new Employee for every incoming Employee from the DB
+             rs.next() ITERATES through the list of data. It will return false when there's no more data
+              In other words, rs.next() returns true as long as there is new data to look at */
+            while(rs.next()){
+
+                //For every Employee record returned from the DB, make a new Employee object
+                Employee employee = new Employee(
+                        rs.getInt("employee_id"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rDAO.getRoleById(rs.getInt("role_id_fk"))
+                        //we needed to retrieve a Role object this way, because the DB just returns the int PK
+                        //"use the getRoleById method to return a role object using the role_id_fk"
+                );
+
+                //for every new Employee, add it to the ArrayList
+                employeeList.add(employee);
+
+                //by the time this while loop breaks (when rs.next() == false), our ArrayList will be full
+
+            }
+
+            return employeeList; //after all that nonsense above, we return our fully populated ArrayList
 
         } catch(SQLException e){
             System.out.println("Failed to get all employees");
