dab66905ef95c2ec5be7aaf90c9c9bc5de472b9b
bryanserfozo <bryan.serfozo@revature.com>
2023-06-05 16:49:21 -0400

    Adding new handlers for put and delete

--- Week3/JavalinIntro/src/main/java/com/revature/JavalinDriver.java
+++ Week3/JavalinIntro/src/main/java/com/revature/JavalinDriver.java
@@ -29,15 +29,79 @@ public class JavalinDriver {
                     ctx.result(response);
                 })
                 .post("/", ctx -> {
-                   // TODO Add request body to list of names
                     String body = ctx.body();
                     names.add(body);
                     // Collections.sort(names);
                     ctx.result("Name successfully added");
                     ctx.status(201);
                 })
-                .put("/sample-put", ctx -> ctx.result("We use PUT to update records"))
-                .delete("/sample-delete", ctx-> ctx.result("DELETE is used to remove records from our data"))
+                .put("/", ctx -> {
+                    // How do we update the list?
+                    // Check if name is in list
+                    // If so, replace
+
+                    // Left an exercise to the viewer
+                    UpdateNameDAO ud = ctx.bodyAsClass(UpdateNameDAO.class);
+
+                    if (names.contains(ud.previousName)){
+                        names.remove(ud.previousName);
+                        names.add(ud.newName);
+                        ctx.status(200);
+                        ctx.result("Successful update of name!");
+                    } else{
+                        ctx.status(400);
+                        ctx.result(ud.previousName + " was not found!");
+                    }
+
+                })
+                .delete("/", ctx-> {
+                    // To delete a name from our list we need to do two things
+
+                    // First check if it in the list
+                    // Then delete if it is
+                    String name = ctx.body();
+
+                    if(names.contains(name)){
+                        // Name is in list so we delete
+                        names.remove(name);
+                        ctx.status(200);
+                        ctx.result("Successfully deleted: " + name);
+                    } else{
+                        // Name is not in list so this is a bad request
+                        ctx.result("Name is not in list!");
+                        ctx.status(400);
+                    }
+                })
                 .start(7070);
+    }
+}
+
+class UpdateNameDAO{
+    String previousName;
+
+    String newName;
+
+    UpdateNameDAO(String previousName, String newName){
+        this.previousName = previousName;
+        this.newName = newName;
+    }
+
+    public UpdateNameDAO() {
+    }
+
+    public String getPreviousName() {
+        return previousName;
+    }
+
+    public void setPreviousName(String previousName) {
+        this.previousName = previousName;
+    }
+
+    public String getNewName() {
+        return newName;
+    }
+
+    public void setNewName(String newName) {
+        this.newName = newName;
     }
 }
