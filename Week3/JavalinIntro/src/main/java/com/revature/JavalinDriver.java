ac242be38c3c30a662da9b67caac9f7fb2d58618
bryanserfozo <bryan.serfozo@revature.com>
2023-06-05 16:12:48 -0400

    Adding basic http handlers

--- Week3/JavalinIntro/src/main/java/com/revature/JavalinDriver.java
+++ Week3/JavalinIntro/src/main/java/com/revature/JavalinDriver.java
@@ -5,13 +5,18 @@ import io.javalin.Javalin;
 public class JavalinDriver {
 
     public static void main(String[] args) {
-       var app = Javalin.create(/*config*/)
+
+
+        var app = Javalin.create(/*config*/)
                // Method referencing, worry about it later
                // TODO explain method referencing
-                .get("/", SampleController::handleGet)
-                .post("/sample-post", ctx -> {
+               // .get("/", ctx -> ctx.result("Hello World"))
+               .get("/", SampleController::handleGet)
+               .post("/sample-post", ctx -> {
                    ctx.result("You sent a POST request!");
                })
-                .start(7070);
+               .put("/sample-put", ctx -> ctx.result("We use PUT to update records"))
+               .delete("/sample-delete", ctx-> ctx.result("DELETE is used to remove records from our data"))
+               .start(7070);
     }
 }
