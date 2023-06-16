a51718d0f3394048f4fa11305701db1356aff361
bryanserfozo <bryan.serfozo@revature.com>
2023-06-16 15:13:53 -0400

    Adding next round of config for SecurityConfig class

--- Week4/CourseManagementSystem/src/main/java/com/revature/security/SecurityConfig.java
+++ Week4/CourseManagementSystem/src/main/java/com/revature/security/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.revature.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 // Inside of here will be my configuration for a large portion of the spring security setup
@@ -11,8 +16,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // First thing we want to start with is setting up the security filter chain
+    // Inject in our CustomUsersDetailsService
+    private final CustomUserDetailsService customUserDetailsService;
 
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+
+    // First thing we want to start with is setting up the security filter chain
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
@@ -23,5 +36,18 @@ public class SecurityConfig {
                 .httpBasic();
 
         return http.build();
+    }
+
+    // We need to get the AuthenticationManager, so it can actually load users by their username
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    // One other thing I'm going to do since it's important to think about security of passwords
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
