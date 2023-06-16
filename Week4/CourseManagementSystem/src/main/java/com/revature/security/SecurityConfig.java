49d90002324aa0edc3fa9e244e817162a7d44dce
bryanserfozo <bryan.serfozo@revature.com>
2023-06-16 14:49:53 -0400

    Adding start of security config

---
+++ Week4/CourseManagementSystem/src/main/java/com/revature/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.revature.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+// Inside of here will be my configuration for a large portion of the spring security setup
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // First thing we want to start with is setting up the security filter chain
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .authorizeRequests()// Underneath here is where we describe the permissions we want to allow
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+}
