96d965fb8df1c48503292d69f1ffc915fca46dac
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-06-29 10:32:08 -0400

    getting data populated

--- Week6/CourseManagementFrontEnd/Teacher.js
+++ Week6/CourseManagementFrontEnd/Teacher.js
@@ -26,6 +26,18 @@ window.onload = async function(){
             //add the td element to the tr element
             row.appendChild(cell)
 
+            //we will do this^^^ for every column in courses
+            let cell2 = document.createElement("td")
+            cell2.innerText = course.name
+            row.appendChild(cell2)
+
+            let cell3 = document.createElement("td")
+            cell3.innerText = course.description
+            row.appendChild(cell3)
+
+            let cell4 = document.createElement("td")
+            cell4.innerText = course.credits
+            row.appendChild(cell4)
 
             //append the tr (table row) to the tbody (table body)
             //a new row will be added FOR EVERY course that got returned in the fetch()
