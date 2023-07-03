6ea5f9d92a7856be2fba287d9f26935e2b222cc8
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-07-03 16:39:28 -0400

    App.tsx rendered a Parent which rendered a Child in hello-react

--- Week7/hello-react/src/components/ParentComponent/ParentComponent.tsx
+++ Week7/hello-react/src/components/ParentComponent/ParentComponent.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ChildComponent } from "../ChildComponent/ChildComponent";
 
 /* Again, every Class Component has two parameters - <props, and state>
  by leaving the state parameter as "any", we can define whatever object we want to store in the state
@@ -18,7 +19,7 @@ export class ParentComponent extends React.Component<{}, any>{
         return(
             <div>
                 <h1>Hello from the Parent Component! My favorite color is: {this.state.color}</h1>
-                
+                <ChildComponent color={this.state.color} animal={this.state.animal}></ChildComponent>
             </div>
         )
     }
