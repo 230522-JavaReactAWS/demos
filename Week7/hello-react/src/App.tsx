84793cd579b5fcc8defcc2a7430d0273706303b0
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-07-03 15:18:14 -0400

    started hello-react

--- Week7/hello-react/src/App.tsx
+++ Week7/hello-react/src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-/* 
-The App.tsx is the root of our application (at least when it comes to component rendering)
-Most of our coding will be done elsewhere, but the App.tsx lets us control WHAT gets renderd
-*/
+/* The App.tsx is the root of our application (at least when it comes to component rendering)
+Most of our coding will be done elsewhere, but the App.tsx lets us control WHAT gets rendered
+We put the components we want to render in the div of the return() in this file
+We can render standard TSX directly inside the div in the return() */
+
+/* Some TSX quirks:
+ className instead of class attribute 
+ comments in TSX are block comments, like the one we're in */
 function App() {
   return (
     <div className="App">
