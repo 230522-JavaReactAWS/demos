649b90201a5e9bb24776e67de34667fa4f69421e
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-07-05 15:06:18 -0400

    added data in hello-events-routing

---
+++ Week7/hello-events-routing/src/data.ts
@@ -0,0 +1,22 @@
+//This file will hold an Array of data, which contains objects that we'll map (iterate) through
+
+export const data = [
+
+    //three objects of futurama characters and some data associated with them
+    {
+        userId:1,
+        name:"Zoidberg",
+        quote:"woopwoopwoopwoopwoop"
+    },
+    {
+        userId:2,
+        name:"Zapp Brannigan",
+        quote:"when I'm in command, every mission is a suicide mission" 
+    },
+    {
+        userId:3,
+        name:"Hubert Farnsworth",
+        quote:"Good news everyone!"
+    }
+
+]
