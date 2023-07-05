831656a72815edc4bb690b5e17bcf4a7d158d90b
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-07-05 11:31:04 -0400

    started a functional component

--- Week7/hello-react/src/components/PostComponent/PostComponent.tsx
+++ Week7/hello-react/src/components/PostComponent/PostComponent.tsx
@@ -21,7 +21,15 @@ export const PostComponent: React.FC<UserInterface> = (user:UserInterface) => {
     //in function components, we only need return(), no render()
     return(
         <div className="post-container">
+            <div className="profile-container">
+                <img className="profile-pic" src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" alt="PROFILE PIC" />
+                <h4 className="profile-username">{username}</h4>
+            </div>
 
+            <div className="content-container">
+                <h5>{username} says:</h5>
+                <p>SOME DATA WILL GO HERE</p>
+            </div>
         </div>
     )
 
