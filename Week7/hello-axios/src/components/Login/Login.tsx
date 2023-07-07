8beee5c08cc9bdd2254df5a70b7c3af6953ca472
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-07-07 11:48:25 -0400

    POST request and basic store

--- Week7/hello-axios/src/components/Login/Login.tsx
+++ Week7/hello-axios/src/components/Login/Login.tsx
@@ -1,32 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import "./Login.css"
+import axios from 'axios'
 
 export const Login: React.FC<any> = () => {
+
+    //useState hooks to declare some state variables that will hold username and password
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
     //we need useNavigate to allow us to navigate between components... no more manual URL changing!!
     const navigate = useNavigate()
 
+    /* when the user updates the username OR password, this will be called thanks to onChange below 
+    the username OR password state variables will get updated based on the name of the input that's changing*/
+    const gatherInput = (input:any) => {
+        if(input.target.name === "username"){
+            setUsername(input.target.value)
+        } else {
+            setPassword(input.target.value)
+        }
+        console.log(input.target.value)
+    }
+
+
     //this function will gather the user input for username/password, and send a POST request to our backend
     const login = async () => {
 
-        //a bunch of login logic...
+        //send an HTTP POST request with axios, and store the response in a variable that we can use
+        const response = await axios.post("http://localhost:8080/auth/login", {username, password})
+        .then((response) => {
+                //if the login was successful, log the user in and store the JWT
+                console.log(response.data.accessToken)
+                //we can use the useNavigate variable above to switch URLs (thus switching components)
+                navigate("/home")
+            }
+        )
+        .catch((error) => {
+            alert("Login failed! Please try again...")
+        })
+
+        console.log(response)
+
+
+
         //TODO: hey Ben see what happens if we don't have async/await
 
-        //we can use the useNavigate variable above to switch URLs (thus switching components)
-        navigate("/home")
+
 
     }
 
     return(
         <div className="login">
 
-            <h1>Welcome to The Pokemon Exchange</h1>
-            <h3>Sign in the browse for a Pokemon!</h3>
+            <div className="text-container">
+                <h1>Welcome to The Pokemon Exchange</h1>
+                <h3>Sign in the browse for a Pokemon!</h3>
 
-            <button className="login-button" onClick={login}>Login</button>
+                <div className="input-container">
+                    <input type="text" placeholder="username" name="username" onChange={gatherInput}/>
+                </div>
+                <div className="input-container">
+                    <input type="password" placeholder="password" name="password" onChange={gatherInput}/>
+                </div>
+
+                <button className="login-button" onClick={login}>Login</button>
+            </div>
 
         </div>
     )
+    /* TODO: white outer glow */
 
 }
---
+++ Week7/hello-axios/src/store.ts
@@ -0,0 +1,23 @@
+/* This is a rudimentary implementation of a store, which is basically global storage
+any data that you want to store globally (visible to the entire app) can reside here
+look into redux to see a more sophisticated use of a store, but be warned it's a dense topic */
+
+//I'm calling this state because I think of the store as a global state of the entire app
+ export const state:any = {
+    JWT:"",
+
+    //this is an example of how you might store a User object (after parsing the JWT)
+    user: {
+        userId:0,
+        username:"",
+        firstName:"",
+        lastName:""
+    },
+
+    //maybe you want to store an Array of all the user accounts
+    accounts:[]
+ }
+
+ //these defaults will all get populated after the user triggers population 
+ //typically, all of the data the user needs gets populated on login, but it's case by case
+ //in this demo, we're only going to be populating the JWT. see login().
